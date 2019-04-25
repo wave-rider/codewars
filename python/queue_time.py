8e888a84ccefef795223daebf44f5fc1eeb3d29b
wave-rider <wave-rider@users.noreply.github.com>
2019-04-25 20:37:22 +1200

    queue_time

--- python/queue_time.py
+++ python/queue_time.py
@@ -1,7 +1,17 @@
 def queue_time(customers, n):
-    index = 0
-    tills = sorted(customers[0:n])
+    if len(customers) is 0:
+        return 0
+    index = n
+    tills = customers[0:n]
     total = 0
-    #1 for x in customers if x > 0]
-    while index < len[customers]:
-        minimum_time = tills[0]
+    while index < len(customers):
+        minimum_time = min(tills)
+        till_index = 0
+        total += minimum_time
+        while till_index < n:
+            tills[till_index] -= minimum_time
+            if index < len(customers) and tills[till_index] == 0 :
+                tills[till_index] = customers[index]
+                index += 1
+            till_index += 1
+    return total + max(tills)
